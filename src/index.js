8324cacf3d790852d70ad2bb2ab560dbeceec82e
dis500 <barca125@yandex.ru>
2018-11-19 11:23:56 +0300

    Исправил 6.1 ДЗ

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ function delayPromise(seconds) {
  */
 function loadAndSortTowns() {
     return new Promise((resolve) => {
-        const xhr = XMLHttpRequest();
+        const xhr = new XMLHttpRequest();
 
         xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
         xhr.responseType = 'json';
